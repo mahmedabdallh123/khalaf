d45fa2c42d0e2c9966cead28469bf4d5f1603345
mahmed abdallh <medotatch124@gmail.com>
2026-05-24 19:21:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from github import Github, GithubException
 
 # ------------------------------- الإعدادات الثابتة -------------------------------
 APP_CONFIG = {
-    "APP_TITLE": "khalaf- CMMS",
+    "APP_TITLE": "سيستم صيانه مصنع خلف",
     "APP_ICON": "🏭",
     "REPO_NAME": "mahmedabdallh123/khalaf",
     "BRANCH": "main",
